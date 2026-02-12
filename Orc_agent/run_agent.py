bbd03e923d8259a8fac499f39910c98f236971ef
GUKHOJeong <rnrgh135@naver.com>
2026-02-13 05:10:59 +0900

    멀티에이전트 전체적인 틀 구성 완료

--- Orc_agent/run_agent.py
+++ Orc_agent/run_agent.py
@@ -2,32 +2,27 @@
 import os
 from dotenv import load_dotenv
 
-# .env 로드 (API 키 등)
 load_dotenv()
 
 import sys
-# src 폴더를 path에 추가하여 모듈 import 가능하게 함
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from src.Orc_agent.Graph.Main_graph import create_main_graph
-
+os.makedirs("img", exist_ok=True)
 def main():
-    # 1. 그래프 생성
+
     print(">>> 그래프 생성 중...")
     app = create_main_graph()
     
-    # 2. 초기 State 설정
-    # 주의: preprocessing_data와 user_query는 필수입니다.
     initial_state = {
-        "file_path": "fake_marketing_data.csv", # 테스트용 더미 경로
+        "file_path": "fake_marketing_data.csv", 
         "user_query": "데이터의 전반적인 추세를 분석하고 시각화해줘"
     }
     
-    # 3. 그래프 실행
+
     print(f">>> 실행 시작: {initial_state['user_query']}")
     config = {"configurable": {"thread_id": "test_thread_2", "user_id": "test_user"}}
-    
-    # stream 모드로 실행하여 중간 로그 확인
     first = True
     while True:
         if first:
@@ -39,16 +34,25 @@ def main():
                 
         print("\n>>> 실행 완료")
 
-        # 인터럽트 상태 확인 (User Request)
+
         state = app.get_state(config)
         print(f"다음 실행할 노드: {state.next[0] if state.next else 'None'}")
-        input_choice = input("현재 그래프를 보고 피드백 여부를 알려주세요(완료/수정/추가):")
-        if input_choice in ["수정","추가"]:
-            input_feedback = input("피드백 내용을 알려주세요.")
+        if state.next[0] == "Analysis":
+            input_choice = input("현재 그래프를 보고 피드백 여부를 알려주세요(완료/수정/추가):")
+            if input_choice in ["수정","추가"]:
+                input_feedback = input("피드백 내용을 알려주세요.")
+            else:
+                input_feedback = ""
         else:
-            input_feedback = ""
+            input_choice = input("현재 보고서를 보고 결정해주세요.(APPROVE/REJECT):")
+            if input_choice == "REJECT":
+                input_feedback = input("거절 이유를 작성해주세요")
+            else:
+                input_feedback = ""
         app.update_state(config, {"user_choice": input_choice ,"feedback": input_feedback}, as_node=state.next[0])
         for event in app.stream(None, config=config):
-            pass
+            for key, value in event.items():
+                print(f"\n--- Node: {key} ---")
+                print(value)
 if __name__ == "__main__":
     main()
